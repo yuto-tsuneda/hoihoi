e101a5ff41b2eda90283762021ca14ae79171e69
Yuto Tsuneda <yuto.tsuneda@prum.com>
2024-07-15 19:26:15 +0900

    モーダル完成

--- app/public/wp-content/themes/hoihoi/index.js
+++ app/public/wp-content/themes/hoihoi/index.js
@@ -1,5 +1,11 @@
 $(function(){
 
+  $(".button").click(function(){
+    $(".upload__modal--window,.upload__modal--overlay").fadeIn();
+  });
 
+  $(".upload__modal--close").click(function(){
+    $(".upload__modal--window,.upload__modal--overlay").fadeOut();
+  });
 
 });
